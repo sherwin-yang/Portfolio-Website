802c812378d88a106afcfd979cedbf5cb823051c
Sherwin Yang <sher.yang79@gmail.com>
2024-05-30 13:30:58 +0700

    update about me important keywords styling

--- src/components/Home/AboutMe.js
+++ src/components/Home/AboutMe.js
@@ -7,11 +7,11 @@ function AboutMe() {
         <p className="about-me-title">Hi, welcome! 👋🏻</p>
         <p>
           I am an iOS Developer with 2+ years of professional working experience at the 
-          #1 OTT platform in Indonesia. Studied Computer Science in BINUS University Jakarta 
+          <b> #1 OTT</b> platform in Indonesia. Studied Computer Science in BINUS University Jakarta 
           from September 2017 and eligible for my Bachelor degree in March 2021.
         </p>
         <p>
-          Write my first Swift code in February 2020 when I joined Apple Developer Academy @BINUS, 
+          Write my first Swift code in February 2020 when I joined <b>Apple Developer Academy @BINUS</b>, 
           I've developed multiple iOS/iPadOS/watchOS apps and successfully launched  
           <span class="bold-text homeplan-text-color"> Homeplan</span> to the App Store in December 2020
           which took me into the journey of joining Apple Catalyst Program to escelate the product (until August 2021).
